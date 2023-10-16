fe52161ae3c28e216ede657ddc68e415c4ffc86b
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-16 04:42:39 +0100

    chore(relative-paths): Fix for relative paths with github pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '@styles/main.css'
+import '../styles/main.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 const HomePage = () => {
   return (
     <main className='gradient-background flex flex-col justify-start items-center text-white'>
       <h1 className='py-2 text-4xl'>Oliver Ebsworth</h1>
-      <Image src="/images/profile.png" width={256} height={256} alt='Profile Image' className='rounded-full' />
+      <img src="./images/profile.png" width={256} height={256} alt='Profile Image' className='rounded-full' />
       <div id="socials" className='py-4'>
 
       </div>
@@ -20,7 +19,7 @@ const HomePage = () => {
       <div className='w-full border-b-2'></div>
       <p className='py-4 text-lg'>Explore my projects by navigating using the buttons below!</p>
       <div className='pb-4'>
-        <button><Image src="/images/kitchen-chaos.png" width={128} height={128} alt="Kitchen Chaos" className='rounded-full' /></button>
+        <button><img src="./images/kitchen-chaos.png" width={128} height={128} alt="Kitchen Chaos" className='rounded-full' /></button>
       </div>
     </main>
   )
