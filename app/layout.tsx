567983501730c765c5e735cff44e942b14ca6485
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-16 16:09:22 +0100

    feat(layout-page): Moved the content that was on the first page to the layout page so that it shows on all of them. Added a C.V. button.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '../styles/main.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import { AiOutlineMail, AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,34 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + "w-screen h-screen gradient-background flex flex-col justify-start items-center text-white"}>
+        <h1 className='py-3 text-4xl'>Oliver Ebsworth</h1>
+        <img src="./images/profile.png" width={256} height={256} alt='Profile Image' className='rounded-full' />
+        <div id="socials" className='flex py-4 text-2xl'>
+          <Link href="mailto:ebswortholiver@gmail.com" className='px-1'><AiOutlineMail /></Link>
+          <Link href="https://github.com/oebsworth" target='_blank' rel='noreferrer noopener' className='px-1'><AiFillGithub /></Link>
+          <Link href="https://www.linkedin.com/in/oebsworth/" target='_blank' rel='noreferrer noopener' className='px-1'><AiFillLinkedin /></Link>
+          <Link href="https://twitter.com/EbsworthOliver" target='_blank' rel='noreferrer noopener' className='px-1'><AiFillTwitterCircle /></Link>
+        </div>
+        <div className='w-1/2 border-b-2'></div>
+        <p className='w-[45%] py-4 text-lg'>Thank you for visiting my portfolio. I am a dedicated programmer who derives great satisfaction from the process of crafting exceptional digital solutions.</p>
+        <div className='w-1/2 border-b-2'></div>
+        <div className='py-6'>
+          {["projects", "experiences", "skills", "c.v."].map((title, index) => (
+            <Link
+              key={`button_${index}`}
+              href={title == "projects" ? "/" : title == "c.v." ? "./documents/c.v.pdf" : `/${title}`}
+              target={title == "c.v." ? "_blank" : ""}
+              rel={title == "c.v." ? "noreferrer noopener" : ""}
+              className='border-2 mx-2 py-2 px-4 sm:mx-4 sm:px-8 uppercase'
+            >
+              {title}
+            </Link>
+          ))}
+        </div>
+        <div className='w-1/2 border-b-2'></div>
+        {children}
+      </body>
     </html>
   )
 }
