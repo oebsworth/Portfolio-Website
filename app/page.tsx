b25c3a02dd597e9808aae0f25e305781e6505866
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-16 02:38:41 +0100

    feat(prototype-home): Implemented prototype code for the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 const HomePage = () => {
   return (
     <main>
+      <h1>Oliver Ebsworth</h1>
+      <Image/>
+      <div id="socials">
 
+      </div>
+      <div></div>
+      <p>Thank you for visiting my portfolio. I am a dedicated programmer who derives great satisfaction from the process of crafting exceptional digital solutions.</p>
+      <div></div>
+      {["projects", "experience", "skills"].map((title, index) => (
+        <Link key={`button_${index}`} href={title == "projects" ? "/" : `/${title}`}>{title}</Link>
+      ))}
+      <div></div>
+      <p>Explore my projects by navigating using the buttons below!</p>
+      <button><Image/></button>
     </main>
   )
 }
