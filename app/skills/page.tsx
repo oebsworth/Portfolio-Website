3becb778784ee7e4f4e1bd74a9c3391f64a490ea
Oliver Ebsworth <ebswortholiver@gmail.com>
2024-05-20 13:37:20 +0100

    Updated skills so that c.v. and website show the same. Fixed a typescript error.

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -6,48 +6,90 @@ const SkillsPage = () => {
         {
             name: "Programming Languages",
             data: [
-                { name: "C#", level: 7, },
-                { name: "HTML", level: 8, },
-                { name: "CSS", level: 7, },
-                { name: "JavaScript", level: 9, },
-                { name: "TypeScript", level: 5, },
+                { name: "JavaScript", level: 8, },
+                { name: "C#", level: 8, },
                 { name: "Python", level: 7, },
-                { name: "C++", level: 5, },
-            ],
+                { name: "C++", level: 6, },
+                { name: "TypeScript", level: 5 },
+            ]
         },
         {
-            name: "Tools",
+            name: "Programming Frameworks",
             data: [
-                { name: "Visual Studio", level: 5, },
-                { name: "VS Code", level: 7, },
-                { name: "Git", level: 6, },
-                { name: "Photoshop", level: 7, },
-                { name: "Blender", level: 9, },
-                { name: "Figma", level: 7, },
-            ],
-        },
-        {
-            name: "Frameworks and Engines",
-            data: [
-                { name: "Unity", level: 7, },
-                { name: "Unreal-Engine 5", level: 5, },
                 { name: "React.js", level: 7, },
                 { name: "Next.js", level: 8, },
                 { name: "Nuxt.js", level: 5, },
+                { name: "VueJS", level: "3" },
+                { name: "Angular", level: "2" },
+                { name: "Solid", level: "2" },
+                { name: "Stencil", level: "2" },
+                { name: "Svelte", level: "2" },
+                { name: "Electron", level: "4" },
+                { name: "Nextron", level: "5" },
             ],
         },
         {
-            name: "Technolgies",
+            name: "Programming Technolgies",
             data: [
-                { name: "Tailwind CSS", level: 7, },
-                { name: "GSAP", level: 5, },
-                { name: "three.js", level: 6, },
-                { name: "MongoDB Atlas", level: 6, },
-                { name: "Mongoose", level: 7, },
-                { name: "Cloudinary", level: 5, },
-                { name: "Sanity", level: 6, },
+                // Javascript
+                { name: "TailwindCSS", level: 7, },
+                { name: "GSAP", level: 4, },
+                { name: "Three.js", level: 4, },
+                { name: "MongoDB", level: 4, },
+                { name: "Mongoose", level: 6, },
+                { name: "Cloudinary", level: 4, },
+                { name: "Sanity", level: 3, },
+                { name: "JSX", level: "10" },
+                { name: "Axios", level: "7" },
+
+                // Python        
+                { name: "OpenCV", level: "5" },
+                { name: "Numpy", level: "4" },
+                { name: "Tkinter", level: "2" },
+                { name: "customtkinter", level: "3" },
+                { name: "Tensorflow", level: "3" },
             ],
-        }
+        },
+        {
+            name: "Softwares",
+            data: [
+                // 3D Modeling
+                { name: "Blender", level: 9, },
+
+                // Coding
+                { name: "Visual Studio", level: 5, },
+                { name: "VS Code", level: 8, },
+                { name: "Git", level: 7, },
+
+                // Web Development
+                { name: "Figma", level: 6, },
+
+                // Adobe
+                { name: "Adobe Photoshop", level: 7, },
+                { name: "Adobe Illustrator", level: 4, },
+
+                // Microsoft Office
+                { name: "MS Word", level: "7" },
+                { name: "MS Powerpoint", level: "5" },
+                { name: "MS Excel", level: "3" },
+            ],
+        },
+        {
+            name: "Game Engines",
+            data: [
+                { name: "Unity", level: "8" },
+                { name: "Unreal Engine", level: "5" }
+            ],
+        },
+        {
+            name: "Areas of Understanding",
+            data: [
+                { name: "Web Development", level: "8" },
+                { name: "Game Development", level: "6" },
+                { name: "Machine Learning", level: "4" },
+            ],
+        },
+
     ]
 
 
@@ -65,7 +107,7 @@ const SkillsPage = () => {
                     <div key={`section_${sectionIndex}`} className="w-full">
                         <h2 className="py-2">{section.name}</h2>
                         {section.data.map((skill, skillIndex) => {
-                            const skillLevelStyle = getSkillLevelStyle(skill.level);
+                            const skillLevelStyle = getSkillLevelStyle(skill.level as number);
                             return (
                                 <div
                                     key={`skill_${skillIndex}`}
@@ -73,7 +115,7 @@ const SkillsPage = () => {
                                 >
                                     <div className="skill-bar" style={skillLevelStyle}>
                                         <span className="skill-name pl-2">{skill.name}</span>
-                                        <span className="skill-level pr-2">{skill.level}</span>
+                                        <span className="skill-level px-2">{skill.level}</span>
                                     </div>
                                 </div>
                             );
